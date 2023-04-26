39e465e93c06cc84244ba0d81a02142c74fc6eca
Oksana Zakharchenko <oksana.zaharchenko@gmail.com>
2023-04-26 14:24:21 +0300

    fix: rename classes

--- scripts/index.js
+++ scripts/index.js
@@ -6,85 +6,85 @@ body.innerHTML = `
       <textarea class="textarea" id="textarea" cols="30" rows="10"></textarea>
       <div class="keyboard">
         <div class="row">
-          <div class="key backtick">\`</div>
-          <div class="key digit1">1</div>
-          <div class="key digit2">2</div>
-          <div class="key digit3">3</div>
-          <div class="key digit4">4</div>
-          <div class="key digit5">5</div>
-          <div class="key digit6">6</div>
-          <div class="key digit7">7</div>
-          <div class="key digit8">8</div>
-          <div class="key digit9">9</div>
-          <div class="key digit0">0</div>
-          <div class="key minus">-</div>
-          <div class="key equal">=</div>
-          <div class="key backspace">Backspace</div>
+          <div class="key Backquote">\`</div>
+          <div class="key Digit1">1</div>
+          <div class="key Digit2">2</div>
+          <div class="key Digit3">3</div>
+          <div class="key Digit4">4</div>
+          <div class="key Digit5">5</div>
+          <div class="key Digit6">6</div>
+          <div class="key Digit7">7</div>
+          <div class="key Digit8">8</div>
+          <div class="key Digit9">9</div>
+          <div class="key Digit0">0</div>
+          <div class="key Minus">-</div>
+          <div class="key Equal">=</div>
+          <div class="key Backspace">Backspace</div>
         </div>
         <div class="row">
-          <div class="key tab">Tab</div>
-          <div class="key letter-q">q</div>
-          <div class="key letter-w">w</div>
-          <div class="key letter-e">e</div>
-          <div class="key letter-r">r</div>
-          <div class="key letter-t">t</div>
-          <div class="key letter-y">y</div>
-          <div class="key letter-u">u</div>
-          <div class="key letter-i">i</div>
-          <div class="key letter-o">o</div>
-          <div class="key letter-p">p</div>
-          <div class="key left-bracket">[</div>
-          <div class="key right-bracket">]</div>
-          <div class="key backslash">\\</div>
-          <div class="key delete">Del</div>
+          <div class="key Tab">Tab</div>
+          <div class="key KeyQ">q</div>
+          <div class="key KeyW">w</div>
+          <div class="key KeyE">e</div>
+          <div class="key KeyR">r</div>
+          <div class="key KeyT">t</div>
+          <div class="key KeyY">y</div>
+          <div class="key KeyU">u</div>
+          <div class="key KeyI">i</div>
+          <div class="key KeyO">o</div>
+          <div class="key KeyP">p</div>
+          <div class="key BracketLeft">[</div>
+          <div class="key BracketRight">]</div>
+          <div class="key Backslash">\\</div>
+          <div class="key Delete">Del</div>
         </div>
         <div class="row">
-          <div class="key capslock">CapsLock</div>
-          <div class="key letter-a">a</div>
-          <div class="key letter-s">s</div>
-          <div class="key letter-d">d</div>
-          <div class="key letter-f">f</div>
-          <div class="key letter-g">g</div>
-          <div class="key letter-h">h</div>
-          <div class="key letter-j">j</div>
-          <div class="key letter-k">k</div>
-          <div class="key letter-l">l</div>
-          <div class="key semicolon">;</div>
-          <div class="key quote">'</div>
-          <div class="key enter">Enter</div>
+          <div class="key CapsLock">CapsLock</div>
+          <div class="key KeyA">a</div>
+          <div class="key KeyS">s</div>
+          <div class="key KeyD">d</div>
+          <div class="key KeyF">f</div>
+          <div class="key KeyG">g</div>
+          <div class="key KeyH">h</div>
+          <div class="key KeyJ">j</div>
+          <div class="key KeyK">k</div>
+          <div class="key KeyL">l</div>
+          <div class="key Semicolon">;</div>
+          <div class="key Quote">'</div>
+          <div class="key Enter">Enter</div>
         </div>
         <div class="row">
-          <div class="key shift-left">Shift</div>
-          <div class="key letter-z">z</div>
-          <div class="key letter-x">x</div>
-          <div class="key letter-c">c</div>
-          <div class="key letter-v">v</div>
-          <div class="key letter-b">b</div>
-          <div class="key letter-n">n</div>
-          <div class="key letter-m">m</div>
-          <div class="key comma">,</div>
-          <div class="key dot">.</div>
-          <div class="key slash">/</div>
-          <div class="key arrow-up">↑</div>
-          <div class="key shift-right">Shift</div>
+          <div class="key ShiftLeft">Shift</div>
+          <div class="key KeyZ">z</div>
+          <div class="key KeyX">x</div>
+          <div class="key KeyC">c</div>
+          <div class="key KeyV">v</div>
+          <div class="key KeyB">b</div>
+          <div class="key KeyN">n</div>
+          <div class="key KeyM">m</div>
+          <div class="key Comma">,</div>
+          <div class="key Period">.</div>
+          <div class="key Slash">/</div>
+          <div class="key ArrowUp">↑</div>
+          <div class="key ShiftRight">Shift</div>
         </div>
         <div class="row">
-          <div class="key control">Ctrl</div>
-          <div class="key win">Win</div>
-          <div class="key alt">Alt</div>
-          <div class="key space">&nbsp</div>
-          <div class="key alt">Alt</div>
-          <div class="key arrow-left">←</div>
-          <div class="key arrow-down">↓</div>
-          <div class="key arrow-right">→</div>
-          <div class="key control">Ctrl</div>
+          <div class="key ControlLeft">Ctrl</div>
+          <div class="key MetaLeft">Win</div>
+          <div class="key AltLeft">Alt</div>
+          <div class="key Space">&nbsp</div>
+          <div class="key AltRight">Alt</div>
+          <div class="key ArrowLeft">←</div>
+          <div class="key ArrowDown">↓</div>
+          <div class="key ArrowRight">→</div>
+          <div class="key ControlRight">Ctrl</div>
         </div>
       </div>
     </div>
 `;
 
 const keyboard = document.querySelector('.keyboard');
-const id = document.getElementById('textarea');
+const textarea = document.getElementById('textarea');
 
 keyboard.addEventListener('click', (event) => {
   if (
@@ -101,6 +101,11 @@ keyboard.addEventListener('click', (event) => {
     && !event.target.className.includes('win')
     && !event.target.className.includes('tab')
   ) {
-    id.value += event.target.textContent;
+    textarea.value += event.target.textContent;
   }
+});
+
+document.addEventListener('keydown', (event) => {
+  console.log(event.code);
+  textarea.value += event.code;
 });
